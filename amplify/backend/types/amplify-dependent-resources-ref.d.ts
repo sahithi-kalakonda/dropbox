a0255ef1d6bf0f611e4492ccb800c723088cb8a7
sahithi-kalakonda <83745501+sahithi-kalakonda@users.noreply.github.com>
2023-10-19 15:51:52 -0700

    first commit
    date 18th oct

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "dropbox": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3dropboxstoragenewww0f023744": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
